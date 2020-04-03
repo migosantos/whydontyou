d781e57fc3694fba01f96fd49ef35c45df860d54
migosantos <migo.santos@gmail.com>
2020-04-04 00:02:26 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {activityList} from './activityList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        randomActivity: null,
+        inAbout: false
+    }
+
+    getRandomInt = (max) => {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    componentDidMount() {
+        this.moreHandler();
+    }
+
+    moreHandler = () => {
+        let max = activityList.length - 1;
+        let randomInt = this.getRandomInt(max);
+        const oldState = {...this.state};
+        oldState.randomActivity = activityList[randomInt];
+        this.setState(oldState);
+    }
+
+    aboutHandler = () => {
+        this.setState({inAbout: true});
+    }
+
+    homeHandler = () => {
+        this.setState({inAbout: false});
+    }
+
+    render() {
+        let content = !this.state.inAbout ? this.getHomeContent() : this.getAboutContent();
+
+        return (
+            <div class="mainContainer">
+                <div className="toolbar">
+                    <a href="#" onClick={this.aboutHandler} className="item">About</a>
+                    <div className="spacer"></div>
+                </div>
+
+                {content}
+            </div>
+        )
+    };
+
+    getHomeContent = () => {
+        return (
+            <div className="content">
+                <div className="card-container">
+                    <span className="bigText">{this.state.randomActivity} </span>
+                </div>
+                <div>
+                    <button onClick={this.moreHandler} className="button-secondary">MORE</button>
+                </div>
+                <div className="footer">
+                    Whatever you do, do it at home.
+                    <br/>
+                    #flattenthecurve
+                </div>
+            </div>
+        );
+    }
+
+    getAboutContent = () => {
+        return (
+            <div className="content">
+                <div className="card-container-2">
+                    <div> Nowadays, social distancing is life. Here are some things you can do to keep yourself
+                        occupied at home.
+                    </div>
+                    <br/>
+                    <br/>
+                    <div>
+                        Suggestions? <a href="mailto:whydontyoutellus@gmail.com">whydontyoutellus@gmail.com</a>
+                    </div>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <div>
+                        <button onClick={this.homeHandler} className="button-secondary">HOME</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
